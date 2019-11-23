2aa2ab22382f8958e8631bd96ade99e72bb54d69
Jörg Volbers <joerg@joergvolbers.de>
2019-11-23 16:10:39 +0100

    Erste Protoversion steht
     - Alerts werden gespeichert und gelöscht
     - Interaktive Funktionen
     - Auswahl mit priorisierter Auswahl aktiver alerts

--- notmuch-alert.el
+++ notmuch-alert.el
@@ -40,121 +40,338 @@
 
 (require 'notmuch)
 (require 'notmuch-bookmarks)
+(require 'acomplete)
 
+;;; * Alert Object
 
-;; * Adding, Removing, Checking Alerts:
+;; This structure defines an alert. An alert can be hooked into a
+;; bookmark by associating it with the property `alert'. The alert can
+;; then be "checked" via its bookmark. In checking, the bookmark
+;; query, in conjunction with the alert's filter query, will be used
+;; to count any "relevant" mails. This result is then passed to the
+;; alert operator, which determines wether the alert is `active' or
+;; not. The operator function (with the number of counted mails as its
+;; argument) just returns a status: either nil, meaning "inactive
+;; alert", or t, meaning "alert should be raised, there are 'new'
+;; mails". This boolean status will be stored in a separate slot of
+;; the alert object for reuse.
+
+(cl-defstruct notmuch-alert
+  ;; Data slots which change after each update of the alert:
+  count
+  status
+  ;; All for deciding about the status:
+  filter 
+  (operator #'notmuch-alert-count>0-p)
+  ;; Offer standardized information:
+  type
+  description
+  format-string)
+
+;; Alert operators:
+
+(defun notmuch-alert-count>0-p (count)
+  "Check for COUNT being > 0."
+  (when count (> count 0)))
+
+;; Predefined alerts:
+
+(defun notmuch-alert-unread ()
+  "Create a new 'unread mails' alert object."
+  (make-notmuch-alert :filter "tag:unread"
+		      :type  'unread
+		      :description "Check for unread mails"
+		      :format-string "%d unread mails"))
+
+(defun notmuch-alert-any ()
+  "Create a new 'any mails' alert object."
+  (make-notmuch-alert :type  'any
+		      :description "Check for any mails"
+		      :format-string "%d mails"))
+
+(defun notmuch-alert-today ()
+  "Create a new 'mails today' alert object."
+  (make-notmuch-alert :filter "date:today"
+		      :type 'today
+		      :description "Check for mails from today"
+		      :format-string "%d mails from today"))
+
+(defvar notmuch-alerts
+  '(notmuch-alert-unread
+    notmuch-alert-any
+    notmuch-alert-today)
+  "List of available alerts (for interactive selection).
+Each element is a function name. The function has to return an
+alert object.")
+
+;;; * Stuffing Alerts into Bookmarks:
+
+(defun notmuch-alert-maybe-save-bookmark-alist ()
+  "Increase bookmark modification counter and possibly save the bookmark alist."
+  (incf bookmark-alist-modification-count)
+  (when (bookmark-time-to-save-p)
+    (bookmark-save)))
 
 (defun notmuch-alert-set (bookmark alert)
-  "Set fn ALERT in BOOKMARK as an alert."
-  (bookmark-prop-set bookmark 'alert alert))
+  "Install the alert object ALERT in BOOKMARK."
+  (bookmark-prop-set bookmark 'alert alert)
+  (notmuch-alert-maybe-save-bookmark-alist))
 
 (defun notmuch-alert-set-via-buffer (alert &optional buffer)
-  "Install ALERT in the bookmark associated with BUFFER.
+  "Install ALERT in the bookmark associated with BUFFER or current buffer.
 Returns the installed alert or nil if someting went wrong."
   (when-let* ((bm (notmuch-bookmarks-get-buffer-bookmark buffer)))
     (notmuch-alert-set bm alert)
     alert))
 
 (defun notmuch-alert-get (bookmark)
-  "Get the alert of BOOKMARK."
+  "Get the alert object of BOOKMARK."
   (bookmark-prop-get bookmark 'alert))
 
-(defun notmuch-alert-p (bookmark)
-  "Check if BOOKMARK is an alert."
+(defun notmuch-alert-get-via-buffer (&optional buffer)
+  "Return the BUFFER's alert, if defined."
+  (when-let* ((bm (notmuch-bookmarks-get-buffer-bookmark buffer)))
+    (notmuch-alert-get bm)))
+
+(defun notmuch-alert-bm-has-alert-p (bookmark)
+  "Check if BOOKMARK has an alert object."
   (and (notmuch-bookmarks-record-p bookmark)
        (not (null (notmuch-alert-get bookmark)))))
 
-(defun notmuch-not-alert-p (bookmark)
-  "Check that BOOKMARK is NOT an alert."
-  (not (notmuch-alert-p bookmark)))
+(defun notmuch-alert-filter-record (bookmark keys)
+  "Return BOOKMARK without the slots named by KEYS."
+  (let* ((name   (first bookmark))
+	 (record (second bookmark)))
+    (cons name
+	  (list
+	   (seq-filter (lambda (keyval) (not (seq-contains keys (car keyval)))) record)))))
 
-(defun notmuch-alert-to-bookmark (bookmark)
-  "Change BOOKMARK so that it is no more an alert.
-This function actually leaves BOOKMARK mostly unchanged, it just
-removes the alert function."
-  (bookmark-prop-set bookmark 'alert nil))
+(defun notmuch-alert-remove-alert (bookmark)
+  "Remove BOOKMARK's alert object."
+  (notmuch-alert-set-tare bookmark nil)
+  (notmuch-alert-set bookmark nil))
 
-(defun notmuch-alert-query (bookmark)
+(defun notmuch-alert-remove-via-buffer (&optional buffer)
+  "Remove BUFFER's bookmark, if defined."
+  (when-let* ((bm (notmuch-bookmarks-get-buffer-bookmark buffer)))
+    (notmuch-alert-remove-alert bm)))
+
+(defun notmuch-alert-bm-query (bookmark)
   "Return the query associated with BOOKMARK."
-  (cl-assert (notmuch-alert-p bookmark))
+  (cl-assert (notmuch-alert-bm-has-alert-p bookmark))
   (bookmark-prop-get bookmark 'filename))
 
-(defun notmuch-alert-call (bookmark)
-  "Call the alert of BOOKMARK and return its value."
-    (when-let* ((alert-fn (notmuch-alert-get bookmark)))
-      (funcall alert-fn bookmark)))
+(defun notmuch-alert-set-tare (bookmark tare)
+  "Set the tare for BOOKMARK."
+  (bookmark-prop-set bookmark 'tara tare))
 
-;;; * Alerts:
+(defun notmuch-alert-get-tare (bookmark)
+  "Get the tare for BOOKMARK."
+  (bookmark-prop-get bookmark 'tara))
 
-;; Helpful functions for alerts:
+(defun notmuch-alert-status-via-bookmark (bookmark)
+  "Get the BOOKMARK's alert's status."
+  (notmuch-alert-status (notmuch-alert-get bookmark)))
 
-(defun notmuch-alert-count (bookmark &optional filter)
-  "Count the mails matched by QUERY and an additional extra FILTER.
+;;; * Updating Alerts:
 
-Correct count according to the tara value of bookmark, if defined."
+(defun notmuch-alert-full-query (bookmark &optional filter)
+  "Build a query joining BOOKMARK's query and the FILTER query."
+  (concat
+   (when filter "(")
+   (notmuch-alert-bm-query bookmark)
+   (when filter ")")
+   (when filter
+     (concat " AND " filter))))
+
+(defun notmuch-alert-do-count (bookmark &optional filter)
+  "Count the mails matched by BOOKMARK and an additional extra FILTER.
+Correct the count according to the tara value of bookmark, if
+defined."
   (let* ((last-count (bookmark-prop-get bookmark 'tara))
-	 (query (concat
-		 (when filter "(")
-		 (notmuch-alert-query bookmark)
-		 (when filter ")")
-		 (when filter
-		   (concat " AND " filter))))
+	 (query      (notmuch-alert-full-query bookmark filter))
 	 (count (string-to-number
 		 (notmuch-command-to-string "count" query))))
-    (- count (or last-count 0))))
+    (max (- count (or last-count 0)) 0)))
 
-;; Some predefined alerts:
-
-;;; TODO Ändern. Der Fehler hier ist: Es geht nicht um "types",
-;;; sondern um spezifische Ergebnisse je nach Funktionsnamen. Also so
-;;; vorgehen: Überlegen, was für fn-spezifische infos wir brauchen;
-;;; dann eine Alist mit den entsprechenden Daten erstellen. Dann ein
-;;; System, dass neue Alerts explizit "registriert" und sie so der
-;;; Datenbank hinzufügt. Dann schließlich eine Funktion, die alle
-;;; vorhandenen Bookmarks auf "legitime types" prüft und ggf. löscht.
-;;; Außerdem wollen wir eine Auswahl aus den Alerts geben.
-;;
-;; ODER: Oder wir packen das alles in die Alert-Funktion. Aber das
-;; wäre dann wie ein Objekt.
-;; HMMMMM.
-;;
-
-(defun notmuch-alert-type-string (bookmark)
-  "Return a string representing the alert type of BOOKMARK."
-  (cl-case (notmuch-alert-get bookmark)
-    ('notmuch-alert-when-unread "unread")
-    ('notmuch-alert-any-new-alert "new")
-    (t "(unknown alert type)")))
-
-(defun notmuch-alert-when-unread (bookmark)
-  "Return the number of unread mails of BOOKMARK."
-  (notmuch-alert-count bookmark "tag:unread"))
-
-(defun notmuch-alert-any-new (bookmark)
-  "Return the number of any mails displayed in BOOKMARK."
-  (notmuch-alert-count bookmark))
-
-;;; * Interactive Functions to work with Alerts:
-
-(defun notmuch-alert-install-unread-alert (&optional buffer interactive)
-  "Install an unread alert in current buffer."
-  (interactive (list (current-buffer) t))
-  (if (notmuch-alert-set-via-buffer #'notmuch-alert-when-unread buffer)
-      (message "Installed unread mails alert.")
-    (when interactive
-      (user-error "Current buffer is not bookmarked."))))
-
-(defun notmuch-alert-check-alert (buffer &optional display)
-  "Check the alert for BUFFER and optionally display its status."
-  (interactive (list (current-buffer) t))
-  (if-let* ((bm (notmuch-bookmarks-get-buffer-bookmark buffer)))
-      (let* ((status (notmuch-alert-call bm)))
-	(when (and status display)
-	  (message (concat "%s " (notmuch-alert-type-string bm)) status))
+(defun notmuch-alert-update (bookmark)
+  "Update the alert of BOOKMARK and return its status."
+  (if-let* ((alert (notmuch-alert-get bookmark)))
+      (let* ((query   (notmuch-alert-filter alert))
+	     (count   (notmuch-alert-do-count bookmark query))
+	     (fn      (notmuch-alert-operator alert))
+	     (status  (funcall fn count)))
+	(setf (notmuch-alert-count alert)  count)
+	(setf (notmuch-alert-status alert) status)
 	status)
-    (when display
-      (user-error "No bookmarked buffer."))))
+    (user-error "No alert defined for bookmark '%s'"
+		(bookmark-name-from-full-record bookmark))))
 
+(defun notmuch-alert-update-all ()
+  "Update all alerts and return all bookmarks with active alerts."
+  (seq-filter #'notmuch-alert-update (seq-filter #'notmuch-alert-bm-has-alert-p bookmark-alist)))
+
+;;; * Information on alerts
+
+(defun notmuch-alert-status-string (alert &optional inactive-string prefix)
+  "Return the status of ALERT as an informative string.
+If alert is not active, return INACTIVE-STRING."
+  (if (notmuch-alert-status alert)
+      (format (concat prefix (notmuch-alert-format-string alert))
+	      (notmuch-alert-count alert))
+    inactive-string))
+
+(defun notmuch-alert-pretty-bm-status-string (bookmark)
+  "Return a pretty status information on BOOKMARK's alert."
+  (let* ((alert  (notmuch-alert-get bookmark))
+	 (status (notmuch-alert-status alert)))
+    (concat (if status
+		(propertize "  ACTIVE" 'face '(:foreground "green"))
+	      (propertize "INACTIVE" 'face '(:foreground "dimgrey")))
+	    " "
+	    (bookmark-name-from-full-record bookmark)
+	    (notmuch-alert-status-string alert nil " -- "))))
+
+(defun notmuch-alert-display-info (alert)
+  "Display information about ALERT in the echo area."
+  (let* ((status (notmuch-alert-status alert))
+	 (tara   (or (notmuch-alert-tara alert) 0)))
+    (message 
+     (concat "Alert"
+	     " '" (or (notmuch-alert-description alert) "no description") "' "
+	     "is " (if status "ACTIVE" "INACTIVE")
+	     ". "
+	     (notmuch-alert-status-string alert nil "Counted ")
+	     (when (> tara 0)
+	       (format " (ignoring %s mails as tare)" tara))
+	     "."
+	     ))))
+
+;;; * Interactive Functions
+
+(defun notmuch-alert-display-info-via-buffer (&optional buf)
+  "Display information about BUF's alert in the echo area."
+  (let* ((alert (notmuch-alert-get-via-buffer buf)))
+    (if alert
+	(notmuch-alert-display-info alert)
+      (user-error "No alert defined"))))
+
+;;;###autoload 
+(defun notmuch-alert-display ()
+  "Display information about the current buffer's alert."
+  (interactive)
+  (notmuch-alert-display-info-via-buffer))
+
+(defun notmuch-alert-select-one (prompt alerts)
+  "Choose between one of the ALERTS."
+  (acomplete prompt alerts
+	     :string-fn #'notmuch-alert-description))
+
+;;;###autoload
+(defun notmuch-alert-visit-alert-bookmark ()
+  "Jump to one of the bookmarks with an alert."
+  (interactive)
+  (notmuch-alert-update-all)
+  (let* ((collection (seq-sort-by #'notmuch-alert-status-via-bookmark
+				  (lambda (a b) (not (or a b)))
+				  (seq-filter #'notmuch-alert-bm-has-alert-p bookmark-alist))))
+    (unless collection
+      (user-error (if include-inactive-alerts "No alerts defined" "All alerts are inactive")))
+    (bookmark-jump (acomplete "Select bookmark: " collection
+			      :string-fn #'notmuch-alert-pretty-bm-status-string))))
+
+;;;###autoload
+(defun notmuch-alert-of-interest ()
+  (interactive)
+  (notmuch-alert-update-all)
+  (let* ((alerts 
+  (seq-sort))
+
+
+
+;; Useful macro for dealing with indirect access to alerts via the current buffer:
+(defmacro notmuch-alert-with-current-buffer (bookmark-symbol alert-symbol &rest body)
+  "Execute BODY with current buffer's bookmark and alert bound to BOOKMARK-SYMBOL and ALERT-SYMBOL."
+  (declare (indent 2)
+	   (debug (symbolp symbolp &rest form)))
+  (let* ((_bm     (intern (symbol-name bookmark-symbol)))
+	 (_alert  (intern (symbol-name alert-symbol))))
+    `(let* ((,_bm (notmuch-bookmarks-get-buffer-bookmark)))
+       (if (null ,_bm)
+	   (user-error "Current buffer is not bookmarked")
+	 (let* ((,_alert (notmuch-alert-get ,_bm)))
+	   (if (null ,_alert)
+	       (user-error "Current buffer's bookmark has no alert")
+	     ,@body))))))
+
+;;;###autoload
+(defun notmuch-alert-install ()
+  "Install an alert for the current buffer.
+The available alerts are listed in `notmuch-alerts'."
+  (interactive)
+  (let* ((bm (notmuch-bookmarks-get-buffer-bookmark)))
+    (if (null bm)
+	(user-error "Current buffer is not bookmarked")
+      (let* ((alert (notmuch-alert-select-one
+		     "Set alert for current buffer:"
+		     ;; always create new objects:
+		     (seq-map #'funcall notmuch-alerts))))
+	(if (null alert)
+	    (user-error "Canceled")
+	  (notmuch-alert-set bm alert)
+	  (message "Set alert '%s'"
+		   (notmuch-alert-description alert)))))))
+
+;;;###autoload
+(defun notmuch-alert-uninstall ()
+  "Uninstall an alert for the current buffer."
+  (interactive)
+  (notmuch-alert-with-current-buffer bm alert
+    (notmuch-alert-remove-alert bm)
+    (message "Alert removed")))
+
+;;;###autoload
+(defun notmuch-alert-check ()
+  "Check current buffer's alert."
+  (interactive)
+  (notmuch-alert-with-current-buffer bm alert
+    (if (notmuch-alert-update bm)
+	(notmuch-alert-display-info alert)
+      (message "Alert inactive"))))
+
+;;;###autoload
+(defun notmuch-alert-tare (&optional just-display)
+  "Set the tare for the current buffer.
+With prefix, just display the current value.
+With double prefix, remove the tare."
+  (interactive "P")
+  (notmuch-alert-with-current-buffer bm alert
+    (let* ((count      (or (notmuch-alert-count alert)
+		 	    (notmuch-alert-update bm)))
+	   (tara       (notmuch-alert-get-tare bm)))
+      (cl-case (car just-display)
+	(16   (progn
+		(notmuch-aler-set-tare bm 0)
+		(message "Tare reset to 0.")))
+	(4    (message (if tara
+			   (format "Current tare is %d" tara)
+			 "No tare set")))
+	(t (progn
+	     (notmuch-alert-set-tare bm count)
+	     (message "Tare set to %d" count)))))))
+
+;;; Convenience functions
+
+(defun notmuch-alert-copy-iff-bookmark (bookmark)
+  "Copy bookmark if it is a notmuch bookmark, else pass it unchanged."
+  (if (notmuch-bookmarks-record-p bookmark)
+      (notmuch-bookmarks-copy-bookmark bookmark)
+    bookmark))
+
+(defun notmuch-alert-remove-all-alerts ()
+  "Remove all ALERTS from the bookmark list, keeping the bookmarks."
+  (setq bookmark-alist
+	(seq-map #'notmuch-alert-copy-iff-bookmark bookmark-alist)))
 
 (provide 'notmuch-alert)
 ;;; notmuch-alert.el ends here
