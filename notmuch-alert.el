43fbf95d269a9ecf83d28f35fb0baf2335e908c9
Jörg Volbers <joerg@joergvolbers.de>
2019-11-24 21:24:08 +0100

    Auswahl von Bookmarks nun mit schönem Prettyprinter

--- notmuch-alert.el
+++ notmuch-alert.el
@@ -41,6 +41,33 @@
 (require 'notmuch)
 (require 'notmuch-bookmarks)
 (require 'acomplete)
+;;; * Global Variables
+
+(defvar notmuch-alert-bm-prettyprint-scheme
+  nil
+  "Scheme to pretty print bookmark items in interactive selections.
+The value has to be a list with max. two elements. The first,
+mandatory element is the name of a function. This function
+recieves two arguments, the bookmark object and its associated
+alert, if any; and returns a string. The second, optional element
+is used to specify some further modifications of the string. This
+second element is a property list. 
+
+   ((functionname (:prop val :prop2 val2 ...)
+    (functionname)
+    (functionname (:prop val))))
+
+The property list (second element) currently accepts two
+properties. If you want to apply several modifications, use one
+list with all properties and values combined.
+
+  (:width <n>) - The total maximum size of the string, to which
+  it is trimmed, if necessary.
+
+  (:face (facespecs...)) - Face specifications which will be
+  applied to the string using `propertize'.
+
+See also `notmuch-alert-pp-column'.")
 
 ;;; * Alert Object
 
@@ -138,6 +165,18 @@ Returns the installed alert or nil if someting went wrong."
   (and (notmuch-bookmarks-record-p bookmark)
        (not (null (notmuch-alert-get bookmark)))))
 
+(defun notmuch-alert-bm-has-active-alert-p (bookmark)
+  "Check if BOOKMARK has an active alert object."
+  (and (notmuch-bookmarks-record-p bookmark)
+       (when-let* ((alert (notmuch-alert-get bookmark)))
+	 (notmuch-alert-status alert))))
+
+(defun notmuch-alert-bm-has-inactive-alert-p (bookmark)
+  "True if BOOKMARK as an inactive alert."
+  (and (notmuch-bookmarks-record-p bookmark)
+       (when-let* ((alert (notmuch-alert-get bookmark)))
+	 (not (notmuch-alert-status alert)))))
+
 (defun notmuch-alert-filter-record (bookmark keys)
   "Return BOOKMARK without the slots named by KEYS."
   (let* ((name   (first bookmark))
@@ -184,20 +223,24 @@ Returns the installed alert or nil if someting went wrong."
    (when filter
      (concat " AND " filter))))
 
+(defun notmuch-alert-notmuch-count (query)
+  "Query the notmuch database and return the result as an integer."
+  (string-to-number
+   (notmuch-command-to-string "count" query)))
+  
 (defun notmuch-alert-do-count (bookmark &optional filter no-correction)
   "Count the mails matched by BOOKMARK and an additional extra FILTER.
 Correct the count according to the tara value of bookmark, if
 defined. This correction can be turned of by setting NO-CORRECTION."
   (let* ((last-count (bookmark-prop-get bookmark 'tara))
 	 (query      (notmuch-alert-full-query bookmark filter))
-	 (count (string-to-number
-		 (notmuch-command-to-string "count" query))))
+	 (count      (notmuch-alert-notmuch-count query)))
     (if no-correction
 	count
     (max (- count (or last-count 0)) 0))))
 
 (defun notmuch-alert-update (bookmark)
-  "Update the alert of BOOKMARK and return its status."
+  "Update the alert of BOOKMARK and return its updated status."
   (if-let* ((alert (notmuch-alert-get bookmark)))
       (let* ((query   (notmuch-alert-filter alert))
 	     (count   (notmuch-alert-do-count bookmark query))
@@ -213,6 +256,117 @@ defined. This correction can be turned of by setting NO-CORRECTION."
   "Update all alerts and return all bookmarks with active alerts."
   (seq-filter #'notmuch-alert-update (seq-filter #'notmuch-alert-bm-has-alert-p bookmark-alist)))
 
+;;; * Prettyprinting Alerts and Bookmarks
+
+;; The functions for the columns:
+
+(defface notmuch-alert-state-bookmark
+  '((t . (:inherit font-lock-builtin-face)))
+  "Face for the status string 'BOOKMARK'.")
+
+(defface notmuch-alert-state-active-alert
+  '((t . (:foreground "green" :weight bold)))
+  "Face for the status string 'ACTIVE'.")
+
+(defface notmuch-alert-state-inactive-alert
+  '((t . (:foreground "dimgrey" :weight bold)))
+  "Face for the status string 'INACTIVE'.")
+
+(defun notmuch-alert-pprinter:state (bookmark alert)
+  "Return colorized status string for BOOKMARK and ALERT."
+  (if (not alert)
+      (propertize "BOOKMARK" 'face 'notmuch-alert-state-bookmark)
+    (if (notmuch-alert-status alert)
+	(propertize "ACTIVE" 'face 'notmuch-alert-state-active-alert)
+      (propertize "INACTIVE" 'face 'notmuch-alert-state-inactive-alert))))
+
+(defun notmuch-alert-pprinter:count (bookmark alert)
+  "Return the total count matching BOOKMARK and ALERT.
+If an alert exists, just display its current count, else query
+the notmuch database directly."
+  (let* ((count (if alert
+		    (notmuch-alert-count alert)
+		  (notmuch-alert-notmuch-count (notmuch-alert-bm-query bookmark)))))
+    (concat "(" (notmuch-hello-nice-number count) ")")))
+
+(defun notmuch-alert-pprinter:alert-info (bookmark alert)
+  "Return information on ALERT."
+  (when alert
+    (notmuch-alert-status-string alert
+				 (notmuch-alert-description alert))))
+
+(defun notmuch-alert-pprinter:bm-name (bookmark alert)
+  "Return the BOOKMARK's name."
+  (bookmark-name-from-full-record bookmark))
+
+(defun notmuch-alert-pprinter:bm-tare (bookmark alert)
+  "Return information on the BOOKMARK's tare setting."
+  (when-let* ((tare (notmuch-alert-get-tare bookmark)))
+    (unless (<= tare 0)
+      (format "Tare set to %d." tare))))
+
+(setq notmuch-alert-bm-prettyprint-scheme
+      '((notmuch-alert-pprinter:state       (:width 8))
+	(" ")
+	(notmuch-alert-pprinter:count       (:width 8))
+	(" ")
+	(notmuch-alert-pprinter:bm-name     (:width 40))
+	(notmuch-alert-pprinter:alert-info  (:concat-if "."))
+	(" ")
+	(notmuch-alert-pprinter:bm-tare)))
+
+;; The pretty printing infrastructure:
+
+(defun notmuch-alert-pp-column (bookmark pprinter mods)
+  "Pretty print BOOKMARK by passing it to PPRINTER and applying MODS.
+PPRINTER is a function which accepts two arguments, the bookmark
+and an alert object (which might be nil). 
+
+As a special case, PPRINTER can also be a string, which is then
+used directly.
+
+MODS is a property list and should be either NIL or one of the
+following:
+
+ (:width <n>)
+ (:face (facespec))
+ (:concat-if \"string\")
+"
+  (let* ((alert (notmuch-alert-get bookmark))
+	 (s     (if (stringp pprinter) pprinter
+		  (funcall pprinter bookmark alert))))
+    (if (null mods)
+	s
+      (when-let* ((concat-if (plist-get mods :concat-if)))
+	(when s
+	  (setq s (concat s concat-if))))
+      (when-let* ((width (plist-get mods :width)))
+	(let* ((pad (- width (string-width s))))
+	  (if (<= pad 0)
+	      (setq s (substring s 0 width))
+	    (setq s (concat s (make-string pad ?\s))))))
+      (when-let* ((face-specs (plist-get mods :face)))
+	(setq s (funcall #'propertize s 'face face-specs))))
+    s))
+
+(defun notmuch-alert-pp-line (scheme bookmark)
+  "Pretty print BOOKMARK using the columns defined in SCHEME.
+SCHEME is a list of cells, each defining a new column (see
+`notmuch-alert-pp-column'.)
+Returns a string."
+  (string-join 
+   (seq-map (lambda (scheme-spec)
+	      (notmuch-alert-pp-column bookmark (first scheme-spec) (second scheme-spec)))
+	    scheme)))
+
+;; TODO Unused, delete me.
+(defun notmuch-alert-bm-pp-all (scheme collection)
+  "Convert a COLLECTION of bookmarks to a pretty printed list of strings, using SCHEME.
+For the structure of scheme, see the documentation of
+`notmuch-alert-bm-prettyprint-scheme'."
+  (seq-map (apply-partially #'notmuch-alert-pp-line scheme) collection))
+
+
 ;;; * Information on alerts
 
 (defun notmuch-alert-status-string (alert &optional inactive-string prefix)
@@ -222,43 +376,6 @@ If alert is not active, return INACTIVE-STRING."
       (format (concat prefix (notmuch-alert-format-string alert))
 	      (notmuch-alert-count alert))
     inactive-string))
-
-(defun notmuch-alert-pretty-bm-status-string (bookmark)
-  "Return a pretty status information on BOOKMARK's alert."
-  (let* ((alert       (notmuch-alert-get bookmark))
-	 (status      (when alert (notmuch-alert-status alert))))
-    ;;
-    (concat
-     ;; Status string
-     (let* ((status-string (cond
-			    ((not alert)    "BOOKMARK")
-			    ((null status)  "INACTIVE")
-			    (t              "  ACTIVE"))))
-       (propertize status-string 'face `(:foreground
-					 ,(cond
-					   ((not alert)   font-lock-builtin-face)
-					   ((null status) "dimgrey")
-					   (t             "green")))))
-     ;; spacer
-     " "
-     ;; total count
-     (let* ((total-count  (notmuch-alert-do-count bookmark nil t))
-	    (total-string (concat "(" (notmuch-hello-nice-number (or total-count 0)) ")"))
-	    (pad          (- 8 (string-width total-string)))) ;; 8 = 6 for the number + 2 for the parentheses
-       (concat
-	total-string
-	(when (> pad 0)
-	  (make-string pad 32)))) ;; good ole' ascii... should be (string-to-char " ")
-     ;; bookmark name
-     " "
-     (bookmark-name-from-full-record bookmark)
-     ;; optionally alert status
-     (when alert (notmuch-alert-status-string alert
-					      (concat " -- " (notmuch-alert-description alert)
-						      (let* ((tare (notmuch-alert-get-tare bookmark)))
-							(when tare
-							  (format ", tare set to %d." tare))))
-					      " -- ")))))
 
 (defun notmuch-alert-display-info (alert)
   "Display information about ALERT in the echo area."
@@ -274,6 +391,16 @@ If alert is not active, return INACTIVE-STRING."
 	       (format " (ignoring %s mails as tare)" tara))
 	     "."
 	     ))))
+
+(defun notmuch-alert-set-sensible-buffer-name ()
+  "Set a better buffer name for the currently visited notmuch show buffer."
+  (when (eq major-mode 'notmuch-show-mode)
+    (let* ((from (notmuch-show-get-from))
+	   (subject (notmuch-show-get-subject))
+	   (new-name (format "%s: %s" from subject))
+	   (unique-p (null (get-buffer new-name))))
+      (when unique-p
+	(setf (buffer-name) new-name)))))
 
 ;;; * Interactive Functions
 
@@ -300,18 +427,14 @@ If alert is not active, return INACTIVE-STRING."
   "Jump to one of the bookmarks with an alert."
   (interactive)
   (notmuch-alert-update-all)
-  (let* ((collection (seq-sort-by #'notmuch-alert-status-via-bookmark
-				  (lambda (a b) (not (or a b)))
-				  (seq-filter #'notmuch-alert-bm-has-alert-p bookmark-alist))))
-    (unless collection
-      (user-error (if include-inactive-alerts "No alerts defined" "All alerts are inactive")))
-    (bookmark-jump (acomplete "Select bookmark: "
-			      (append collection
-				      (seq-filter (lambda (bm)
-						    (and (notmuch-bookmarks-record-p bm)
-							 (not (notmuch-alert-bm-has-alert-p bm))))
-						  bookmark-alist))
-			      :string-fn #'notmuch-alert-pretty-bm-status-string))))
+  (let* ((collection (append
+		      (seq-filter #'notmuch-alert-bm-has-active-alert-p bookmark-alist)
+		      (seq-filter #'notmuch-alert-bm-has-inactive-alert-p bookmark-alist)
+		      (seq-filter (lambda (bm) (and (notmuch-bookmarks-record-p bm)
+						    (not (notmuch-alert-bm-has-alert-p bm))))
+				  bookmark-alist))))
+    (bookmark-jump (acomplete "Select bookmark: " collection
+			      :string-fn (apply-partially #'notmuch-alert-pp-line notmuch-alert-bm-prettyprint-scheme)))))
 
 ;; Useful macro for dealing with indirect access to alerts via the current buffer:
 (defmacro notmuch-alert-with-current-buffer (bookmark-symbol alert-symbol &rest body)
